2c4ed0535d105269b34e7712b0bb6e146d7971e0
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-02-02 15:02:23 +0200

    plugins: add nvim-cmp plugin

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,16 @@
+return {
+  -- Completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    version = false,
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      require("plugins.configs.cmp").config()
+    end,
+  },
+}
---
+++ lua/plugins/configs/cmp.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+function M.config()
+  local cmp = require("cmp")
+  local opts = {
+    mapping = cmp.mapping.preset.insert({
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "path" },
+    }, {
+      { name = "buffer" },
+    }),
+  }
+  cmp.setup(opts)
+end
+
+return M

64a7c934f40cc125c4944644db7e728065a3a671
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-02-02 17:44:02 +0200

    keymaps: add telescope mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,8 @@ local map = vim.keymap.set
 -- Tabs
 map({ "n", "t" }, "<M-n>", "<cmd>tabnext<CR>")
 map({ "n", "t" }, "<M-p>", "<cmd>tabprev<CR>")
+
+-- Telescope
+local telescope_builtin = require("telescope.builtin")
+map("n", "<leader>ff", telescope_builtin.find_files)
+map("n", "<leader>fw", telescope_builtin.live_grep)

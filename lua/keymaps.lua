9d45c064805678de3ce3ce51d331ab62bc3e3797
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-02-02 17:56:11 +0200

    keymaps: add lsp mappings

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,3 +14,25 @@ map("n", "<F2>", "<cmd>NvimTreeToggle<CR>")
 
 -- Fugitive
 map("n", "<leader>gg", "<cmd>tab Git<CR>")
+
+-- LSP
+map("n", "<space>e", vim.diagnostic.open_float)
+map("n", "[d", vim.diagnostic.goto_prev)
+map("n", "]d", vim.diagnostic.goto_next)
+map("n", "<space>q", vim.diagnostic.setloclist)
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspKeymaps", {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    map("n", "gD", vim.lsp.buf.declaration, opts)
+    map("n", "gd", vim.lsp.buf.definition, opts)
+    map("n", "gt", vim.lsp.buf.type_definition, opts)
+    map("n", "gi", vim.lsp.buf.implementation, opts)
+    map("n", "gr", vim.lsp.buf.references, opts)
+    map("n", "K", vim.lsp.buf.hover, opts)
+    map("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    map("n", "<leader>f", vim.lsp.buf.format, opts)
+    map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+  end,
+})

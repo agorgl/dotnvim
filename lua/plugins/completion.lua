dcd4a8a352fca3d8fca8bb2096bada5c2f803e29
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-02-02 15:51:28 +0200

    plugins: add luasnip plugin

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,9 +8,12 @@ return {
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
+      "saadparwaiz1/cmp_luasnip",
     },
     config = function()
       require("plugins.configs.cmp").config()
     end,
   },
+  -- Snippet engine
+  "L3MON4D3/LuaSnip",
 }

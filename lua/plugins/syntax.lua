3078670ca6d4a720cd7dc2f027d74eb1190ae9b1
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-02-03 15:43:54 +0200

    plugins: treesitter: add setup

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -4,6 +4,13 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "VeryLazy",
+    main = "nvim-treesitter.configs",
+    opts = {
+      ensure_installed = "all",
+      highlight = {
+        enable = true,
+      },
+    },
   },
   -- Buffer option heuristics
   "tpope/vim-sleuth",

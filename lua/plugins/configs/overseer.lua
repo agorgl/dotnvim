3535f8ba257077226d26e3615508a87c5be354ee
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-02-04 14:59:32 +0200

    plugins: overseer: add run toggle user command

--- lua/plugins/configs/overseer.lua
+++ lua/plugins/configs/overseer.lua
@@ -12,6 +12,20 @@ function M.config()
     },
   }
   overseer.setup(opts)
+
+  vim.api.nvim_create_user_command("OverseerRunToggle", function()
+    local tasks = overseer.list_tasks()
+    if not vim.tbl_isempty(tasks) then
+      local task = tasks[1]
+      if task.strategy.name == "toggleterm" then
+        local term = task.strategy.term
+        term:toggle()
+      end
+    else
+      --overseer.run_template({ tags = { overseer.TAG.BUILD } })
+      overseer.run_template({ tags = { "RUN" } })
+    end
+  end, {})
 end
 
 return M

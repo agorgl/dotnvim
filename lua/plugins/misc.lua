f76470a6211f7c55a6913f4318b59e98f91384f6
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-04-12 23:22:05 +0300

    plugins: markdown-preview: modify build

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,7 +2,9 @@ return {
   -- Markdown
   {
     "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
   },

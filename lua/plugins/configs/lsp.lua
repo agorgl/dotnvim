7672fde648da6b09f01b3d952e16cebbaacf8749
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-08-21 19:42:59 +0300

    plugins: lspconfig: add lua_ls server setup

--- lua/plugins/configs/lsp.lua
+++ lua/plugins/configs/lsp.lua
@@ -191,6 +191,31 @@ function M.setup_language_servers()
     },
     yamlls = yamlls_config(),
     ansiblels = {},
+    lua_ls = {
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+          return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+          runtime = {
+            version = "LuaJIT",
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+              "${3rd}/luv/library",
+              "${3rd}/busted/library",
+            },
+          },
+        })
+      end,
+      settings = {
+        Lua = {},
+      },
+    },
     tailwindcss = {
       settings = {
         tailwindCSS = {

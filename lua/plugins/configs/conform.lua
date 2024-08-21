ac52e244455646084969ea72daf72fc296096e5c
Loukas Agorgianitis <loukas@agorgianitis.com>
2024-08-21 14:56:53 +0300

    plugins: conform: add commands to toggle format on save

---
+++ lua/plugins/configs/conform.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+function M.config()
+  local conform = require("conform")
+
+  vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+      -- FormatDisable! will disable formatting just for this buffer
+      vim.b.disable_autoformat = true
+    else
+      vim.g.disable_autoformat = true
+    end
+  end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+  })
+  vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+  end, {
+    desc = "Re-enable autoformat-on-save",
+  })
+
+  local opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+    },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+
+      local fmt_opts = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      }
+      return fmt_opts
+    end,
+  }
+  conform.setup(opts)
+end
+
+return M
--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -24,14 +24,9 @@ return {
   {
     "stevearc/conform.nvim",
     event = "BufWritePre",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-    },
+    cmd = { "ConformInfo", "FormatDisable", "FormatEnable" },
+    config = function()
+      require("plugins.configs.conform").config()
+    end,
   },
 }

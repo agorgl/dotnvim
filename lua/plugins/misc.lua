ce83fa5e8d827e28b3c6aafecba472076ddefa9d
Loukas Agorgianitis <loukas@agorgianitis.com>
2025-10-16 09:58:20 +0300

    plugins: markdown-preview: load plugin before build

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,7 +2,8 @@ return {
   -- Markdown
   {
     "iamcco/markdown-preview.nvim",
-    build = function()
+    build = function(plugin)
+      require("lazy").load({ plugins = { plugin.name } })
       vim.fn["mkdp#util#install"]()
     end,
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },

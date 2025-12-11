23278269ea29930873062c26773e4d637190ea35
Loukas Agorgianitis <loukas@agorgianitis.com>
2025-12-11 17:56:21 +0200

    plugins: telescope: add smart_find picker

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local is_inside_work_tree = {}
+
 function M.config()
   local telescope = require("telescope")
   telescope.setup({
@@ -18,6 +20,23 @@ function M.config()
   })
   telescope.load_extension("fzf")
   telescope.load_extension("yaml_schema")
+end
+
+function M.smart_find(opts)
+  opts = opts or {}
+
+  local cwd = vim.fn.getcwd()
+  if is_inside_work_tree[cwd] == nil then
+    vim.fn.system("git rev-parse --is-inside-work-tree")
+    is_inside_work_tree[cwd] = vim.v.shell_error == 0
+  end
+
+  local telescope_builtin = require("telescope.builtin")
+  if is_inside_work_tree[cwd] then
+    telescope_builtin.git_files(opts)
+  else
+    telescope_builtin.find_files(opts)
+  end
 end
 
 return M

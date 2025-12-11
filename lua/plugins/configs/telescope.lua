a01a6cead97f5cf07cb9b800f6e35ded35770caf
Loukas Agorgianitis <loukas@agorgianitis.com>
2025-12-11 17:55:53 +0200

    plugins: telescope: setup picker options

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -2,7 +2,20 @@ local M = {}
 
 function M.config()
   local telescope = require("telescope")
-  telescope.setup()
+  telescope.setup({
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
+      git_files = {
+        show_untracked = true,
+      },
+      live_grep = {
+        hidden = true,
+        additional_args = { "--hidden", "--glob", "!**/.git/*" },
+      },
+    },
+  })
   telescope.load_extension("fzf")
   telescope.load_extension("yaml_schema")
 end

d6e2345bdddb9b908ab6ff49333dcbc53a54ab0d
Loukas Agorgianitis <loukas@agorgianitis.com>
2026-07-01 06:25:10 +0300

    init: add skeleton

---
+++ init.lua
@@ -0,0 +1,17 @@
+--
+-- init
+--
+
+local modules = {
+  "options",
+  "plugins",
+  "keymaps",
+}
+
+for _, m in ipairs(modules) do
+  local ok, result = xpcall(require, debug.traceback, m)
+  if not ok then
+    local msg = string.format("error: init: could not require '%s' module: %s", m, result)
+    vim.notify(msg, vim.log.levels.ERROR)
+  end
+end

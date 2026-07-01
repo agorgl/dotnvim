479c8b5cbfd4ed97843c17b827a5688932a25e8a
Loukas Agorgianitis <loukas@agorgianitis.com>
2026-07-01 10:00:13 +0300

    fzf-lua: add keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,3 +12,9 @@ vim.keymap.set({ "n", "t" }, "<A-p>", "<Cmd>tabprev<CR>")
 
 -- terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- fzf-lua
+vim.keymap.set("n", "<leader>fa", "<Cmd>FzfLua builtin<CR>")
+vim.keymap.set("n", "<leader>ff", "<Cmd>FzfLua files<CR>")
+vim.keymap.set("n", "<leader>fw", "<Cmd>FzfLua live_grep<CR>")
+vim.keymap.set("v", "<leader>fs", "<Cmd>FzfLua grep_visual<CR>")

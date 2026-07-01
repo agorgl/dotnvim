00c6672d590e1ebeea035eb8454e5677870c51dd
Loukas Agorgianitis <loukas@agorgianitis.com>
2026-07-01 08:00:48 +0300

    keymaps: add initial keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,14 @@
 --
 -- keymaps
 --
+
+-- movement
+vim.keymap.set({ "n", "v" }, "j", "gj")
+vim.keymap.set({ "n", "v" }, "k", "gk")
+
+-- tabs
+vim.keymap.set({ "n", "t" }, "<A-n>", "<Cmd>tabnext<CR>")
+vim.keymap.set({ "n", "t" }, "<A-p>", "<Cmd>tabprev<CR>")
+
+-- terminal
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")

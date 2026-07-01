59b91022820b6f7cf97e3f559886541995f865b9
Loukas Agorgianitis <loukas@agorgianitis.com>
2026-07-01 11:15:31 +0300

    conform.nvim: add config

---
+++ lua/plugin/conform_nvim.lua
@@ -0,0 +1,61 @@
+--
+-- plugin.conform_nvim
+--
+
+local M = {}
+
+function M.config()
+  local conform = require("conform")
+  conform.setup({
+    formatters_by_ft = {
+      lua = { "stylua" },
+      c = { "clang-format" },
+    },
+    formatters = {
+      ["clang-format"] = {
+        prepend_args = function()
+          local clang_format_config = ".clang-format"
+          if vim.fs.root(0, clang_format_config) then
+            return nil
+          end
+          local config = vim.api.nvim_get_runtime_file(vim.fs.joinpath("data", clang_format_config), false)[1]
+
+          local style_arg = string.format("--style=file:%s", config)
+          return { style_arg }
+        end,
+      },
+    },
+    format_on_save = function(bufnr)
+      -- disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+
+      local fmt_opts = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      }
+      return fmt_opts
+    end,
+  })
+
+  vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+      -- disable formatting just for this buffer if bang in command
+      vim.b.disable_autoformat = true
+    else
+      vim.g.disable_autoformat = true
+    end
+  end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+  })
+  vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+  end, {
+    desc = "Re-enable autoformat-on-save",
+  })
+end
+
+return M

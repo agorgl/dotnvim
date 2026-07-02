f49d8741b51d2788f38acd43269a30ed5d9fbf31
Loukas Agorgianitis <loukas@agorgianitis.com>
2026-07-02 23:51:38 +0300

    lsp: move lsp configuration to dedicated module

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,26 @@
 --
 -- lsp
 --
+
+local configs = {
+  lua_ls = {
+    settings = {
+      Lua = {
+        runtime = {
+          version = "LuaJIT",
+        },
+        workspace = {
+          preloadFileSize = 10000,
+          library = {
+            vim.env.VIMRUNTIME,
+          },
+        },
+      },
+    },
+  },
+}
+
+for server, config in pairs(configs) do
+  vim.lsp.config(server, config)
+  vim.lsp.enable(server)
+end
--- lua/plugin/nvim_lspconfig.lua
+++ lua/plugin/nvim_lspconfig.lua
@@ -5,28 +5,6 @@
 local M = {}
 
 function M.config()
-  local configs = {
-    lua_ls = {
-      settings = {
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
-          },
-          workspace = {
-            preloadFileSize = 10000,
-            library = {
-              vim.env.VIMRUNTIME,
-            },
-          },
-        },
-      },
-    },
-  }
-
-  for server, config in pairs(configs) do
-    vim.lsp.config(server, config)
-    vim.lsp.enable(server)
-  end
 end
 
 return M

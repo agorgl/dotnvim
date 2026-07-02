50b67aa6df0742671eef02849a7d8d54a3189eef
Loukas Agorgianitis <loukas@agorgianitis.com>
2026-07-02 23:48:09 +0300

    diagnostics: move diagnostics configuration to dedicated module

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,3 +1,27 @@
 --
 -- diagnostics
 --
+
+vim.diagnostic.config({
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "✘",
+      [vim.diagnostic.severity.WARN] = "!",
+      [vim.diagnostic.severity.INFO] = "»",
+      [vim.diagnostic.severity.HINT] = "*",
+    },
+  },
+  severity_sort = true,
+  underline = false,
+  virtual_text = false,
+})
+
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("diagnostic.hold", { clear = true }),
+  callback = function()
+    vim.diagnostic.open_float({
+      severity_sort = true,
+      focusable = false,
+    })
+  end,
+})
--- lua/plugin/nvim_lspconfig.lua
+++ lua/plugin/nvim_lspconfig.lua
@@ -27,30 +27,6 @@ function M.config()
     vim.lsp.config(server, config)
     vim.lsp.enable(server)
   end
-
-  vim.diagnostic.config({
-    signs = {
-      text = {
-        [vim.diagnostic.severity.ERROR] = "✘",
-        [vim.diagnostic.severity.WARN] = "!",
-        [vim.diagnostic.severity.INFO] = "»",
-        [vim.diagnostic.severity.HINT] = "*",
-      },
-    },
-    severity_sort = true,
-    underline = false,
-    virtual_text = false,
-  })
-
-  vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-    group = vim.api.nvim_create_augroup("diagnostic.hold", { clear = true }),
-    callback = function()
-      vim.diagnostic.open_float({
-        severity_sort = true,
-        focusable = false,
-      })
-    end,
-  })
 end
 
 return M

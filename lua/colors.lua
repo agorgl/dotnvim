272813df5db098bfe35e5a6904d9234772625839
Loukas Agorgianitis <loukas@agorgianitis.com>
2026-08-02 23:39:35 +0300

    colors: make background transparent when being embedded

--- lua/colors.lua
+++ lua/colors.lua
@@ -2,5 +2,16 @@
 -- colors
 --
 
+-- embedded background
+if vim.tbl_contains(vim.v.argv, "--embed") then
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    group = vim.api.nvim_create_augroup("colors.embedded.changed", { clear = true }),
+    callback = function(_)
+      local transparent = require("transparent")
+      transparent.toggle(true)
+    end,
+  })
+end
+
 -- colorscheme
 vim.cmd("colorscheme onedark")

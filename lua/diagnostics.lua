743d2ae868fa9d2435b05038ebb24c8ef5ad291f
Loukas Agorgianitis <loukas@agorgianitis.com>
2026-08-10 18:19:10 +0300

    diagnostics: don't show diagnostics popup if other popup menus are visible

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -19,6 +19,15 @@ vim.diagnostic.config({
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
   group = vim.api.nvim_create_augroup("diagnostic.hold", { clear = true }),
   callback = function()
+    if vim.fn.pumvisible() ~= 0 then
+      return
+    end
+
+    local blink_cmp = require("blink.cmp")
+    if blink_cmp.is_menu_visible() then
+      return
+    end
+
     vim.diagnostic.open_float({
       severity_sort = true,
       focusable = false,
